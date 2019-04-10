cf104b74c99333aaa0b60d1c932c08347a26133e
Felix Loesing <loesing@usc.edu>
2019-04-09 23:06:40 -0700

    added DetailsServlet

---
+++ src/myPackage/DetailServlet.java
@@ -0,0 +1,57 @@
+package myPackage;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import classes.DatabaseConnector;
+import classes.Event;
+
+/**
+ * Servlet implementation class DetailServlet
+ */
+@WebServlet("/DetailServlet")
+public class DetailServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DetailServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		int eventID = Integer.valueOf(request.getParameter("eventID"));
+		
+		Event e = DatabaseConnector.getEvent(eventID);
+		
+		request.setAttribute("event", e);
+		request.setAttribute("eventID", eventID);
+		RequestDispatcher dispatch = getServletContext().getRequestDispatcher("/details.jsp");
+		
+		dispatch.forward(request, response);
+
+		return;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
